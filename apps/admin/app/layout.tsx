d7bfdb399fbb789c06a61861316b763dcc4aec1a
伍祥 <wuxiang@5666.net>
2026-04-29 14:17:32 +0800

    feat: next上添加控制台

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import MyBaseLayout from "./providers/base-layout";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "../public/fonts/GeistVF.woff",
@@ -27,6 +28,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="zh-CN">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
+        />
+
+        {/* 控制台 */}
+        <Script
+          strategy="afterInteractive"
+          src="https://cdn.jsdelivr.net/npm/eruda"
+        />
+        <Script
+          strategy="afterInteractive"
+          id="eruda-init"
+          dangerouslySetInnerHTML={{
+            __html: `
+              function initEruda() {
+                if (typeof eruda !== 'undefined') {
+                  eruda.init();
+                } else {
+                  setTimeout(initEruda, 100);
+                }
+              }
+              initEruda();
+            `,
+          }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <MyBaseLayout>{children}</MyBaseLayout>
       </body>
